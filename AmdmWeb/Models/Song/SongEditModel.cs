e13e3d3e916ae2990271d377461d396294fdcca2
Александр Волк <alex_v41@hotmail.com>
2016-09-28 21:02:23 +0300

    transition to VS ALL

---
+++ AmdmWeb/Models/Song/SongEditModel.cs
@@ -0,0 +1,22 @@
+﻿using AmdmData.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AmdmWeb.Models.Song
+{
+    public class SongEditModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string SongPageLink { get; set; }
+        public string Text { get; set; }
+        public int Views { get; set; }
+        public string VideoLink { get; set; }
+        public int PerformerId { get; set; }
+        public string Chords { get; set; }
+        public int Number { get; set; }
+        public SongsSortingTypes SongsSortingType { get; set; }
+    }
+}
---
+++ AmdmWeb/Scripts/client-server.js
@@ -0,0 +1,14 @@
+﻿$(function () {
+    var chat = $.connection.amdmHub;    
+    chat.client.addMessage = function (name, message) {
+        debugger;
+        $('#alertDiv').append('<div class="alert alert-success">'+
+                                '<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>'+
+                                '<strong>' + name +'</strong>' + message + 
+                              '</div>'
+        );        
+    };  
+    $.connection.hub.start().done(function () {});
+});
+
+
